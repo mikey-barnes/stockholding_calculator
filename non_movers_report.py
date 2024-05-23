ebd2646a0752460b3901a5540c154de9ff0192ed
mikey-barnes <67759982+mikey-barnes@users.noreply.github.com>
2024-05-23 07:25:40 +0100

    Run Black code formatter

--- non_movers_report.py
+++ non_movers_report.py
@@ -1,25 +1,30 @@
 import csv
 
-def main():
-    with open("sales_data.csv", errors="ignore", newline='') as sales_csv, open("product-export.csv", errors="ignore", newline='') as products_csv:
 
-        
+def main():
+    with open("sales_data.csv", errors="ignore", newline="") as sales_csv, open(
+        "product-export.csv", errors="ignore", newline=""
+    ) as products_csv:
         sales_reader = csv.DictReader(sales_csv)
         products_reader = csv.DictReader(products_csv)
-        
-        #parse products csv
+
+        # parse products csv
         products_sku_list = list()
         for product in products_reader:
             products_sku_list.append(product["sku"])
-        
-        #parse sales csv
+
+        # parse sales csv
         sales_sku_list = list()
         for product in sales_reader:
             sales_sku_list.append(product["SKU"])
 
-        non_movers_sku_list = [sku for sku in products_sku_list if sku not in sales_sku_list]
+        non_movers_sku_list = [
+            sku for sku in products_sku_list if sku not in sales_sku_list
+        ]
 
-    with open("product-export.csv", errors="ignore", newline='') as products_csv, open("non_movers.csv", "w", newline='') as non_movers_csv:
+    with open("product-export.csv", errors="ignore", newline="") as products_csv, open(
+        "non_movers.csv", "w", newline=""
+    ) as non_movers_csv:
         products_reader = csv.DictReader(products_csv)
         field_names = products_reader.fieldnames
         non_movers_writer = csv.DictWriter(non_movers_csv, fieldnames=field_names)
